3aa051d809ef3683802ab8f1577d10be933649da
homemix <kenmutati@gmail.com>
2021-08-28 13:41:41 +0300

    add app entry point and db config

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var flash = require('express-flash');
+var session = require('express-session');
+var mysql = require('mysql');
+var connection  = require('./lib/db');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var patientsRouter = require('./routes/patients')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +25,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  cookie: { maxAge: 60000 },
+  store: new session.MemoryStore,
+  saveUninitialized: true,
+  resave: 'true',
+  secret: 'secret'
+}))
+
+app.use(flash());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/patients', patientsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
